6409853ba14b345a8f96084f1a3e23cf0384df62
Patrick <pachtrick@gmail.com>
2020-02-13 09:05:51 -0500

    add 10% black brush strokes

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -6,43 +6,14 @@ function getRandomRGB(){
     return [getRandomInt(256),getRandomInt(256),getRandomInt(256)];
 }
 
-
-
 const gridContainer = document.querySelector('#gridContainer');
 let color = getRandomRGB();
 
+let n = 16, s = '', inputSize = 0;
 
-let n = 16, s = '';
-
-
-for (let i = 0; i < n*n; i++) {
-    s += `<div class="cell" style="width: ${500/(n*5)}%; flex-grow: 1; background-color: rgb(${color[0]}, ${color[1]}, ${color[2]});"`;
-    s += '></div>'
-};
-
-
-gridContainer.innerHTML = s;
-
-const cellsArray = [...document.querySelectorAll(".cell")];
-
-cellsArray.forEach(cell => {
-    cell.addEventListener("mouseover", function( event ) {  
-
-        // change color when mousedover target
-        this.style.backgroundColor = "black";
-      }, false);
-});
-
-function resetGrid() {
-    s = '';
-
-    let input = prompt("Enter a new size for the NxN sketchpad (1-64)")
-    while (input < 1 || input > 64) {
-        input = prompt("Enter a new size for the NxN sketchpad (1-64)")
-    }
-
-    for (let i = 0; i < input*input; i++) {
-        s += `<div class="cell" style="width: ${500/(input*5)}%; flex-grow: 1; background-color: rgb(${color[0]}, ${color[1]}, ${color[2]});"`;
+function setUpGrid(grid_size){
+    for (let i = 0; i < grid_size*grid_size; i++) {
+        s += `<div class="cell" style="width: ${500/(grid_size*5)}%; flex-grow: 1; background-color: rgb(${color[0]}, ${color[1]}, ${color[2]});"`;
         s += '></div>'
     };
 
@@ -51,38 +22,43 @@ function resetGrid() {
     const cellsArray = [...document.querySelectorAll(".cell")];
 
     cellsArray.forEach(cell => {
-    cell.addEventListener("mouseover", function( event ) {  
+        cell.addEventListener("mouseover", function( event ) {  
+    
+            // change color when mousedover target
+            let cellColor = this.style.backgroundColor.replace(/[^\d,]/g, '').split(',');
 
-        // change color when mousedover target
-        this.style.backgroundColor = "black";
-      }, false);
-});
+            cellColor[0]-=26;
+            cellColor[1]-=26;
+            cellColor[2]-=26;
+
+            this.style.backgroundColor = `rgb(${cellColor[0]},${cellColor[1]},${cellColor[2]})`;
+            
+          }, false);
+    });
+}
+
+function getGridSize() {
+    while (inputSize < 1 || inputSize > 64) {
+        inputSize = prompt("Enter a new size for the NxN sketchpad (1-64)")
+    }
+}
+
+
+setUpGrid(n);
+
+
+function resetGrid() {
+
+    s = '';
+    inputSize = 0
+
+    getGridSize();
+    setUpGrid(inputSize);
 }
 
 function resetColor() {
 
     color = getRandomRGB();
-    s = '';
-    
-    let input = prompt("Enter a new size for the NxN sketchpad (1-64)")
-    while (input < 1 || input > 64) {
-        input = prompt("Enter a new size for the NxN sketchpad (1-64)")
-    }
 
-    for (let i = 0; i < input*input; i++) {
-        s += `<div class="cell" style="width: ${500/(input*5)}%; flex-grow: 1; background-color: rgb(${color[0]}, ${color[1]}, ${color[2]});"`;
-        s += '></div>'
-    };
-
-    gridContainer.innerHTML = s;
-
-    const cellsArray = [...document.querySelectorAll(".cell")];
-
-    cellsArray.forEach(cell => {
-    cell.addEventListener("mouseover", function( event ) {  
-
-        // change color when mousedover target
-        this.style.backgroundColor = "black";
-      }, false);
-});
+    resetGrid();
 }
